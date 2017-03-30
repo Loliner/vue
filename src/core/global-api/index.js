f24710dd02f1bcd1e2f2cb844d6640a42d572dc8
zhengjiu <zhengjiu@baidu.com>
2017-03-30 12:48:32 +0800

    global-api源码阅读

--- src/core/global-api/index.js
+++ src/core/global-api/index.js
@@ -1,5 +1,6 @@
 /* @flow */
 
+// config中已有默认配置
 import config from '../config'
 import { initUse } from './use'
 import { initMixin } from './mixin'
@@ -16,10 +17,14 @@ import {
   defineReactive
 } from '../util/index'
 
+// 初始化Vue的全局调用接口
+// 此处的 Vue: GlobalAPI 为泛型，指定参数Vue 必须传入GlobalAPI类型（typescript）
+// 如果传入类型不符合，则其在编译成标准es语句时会报错
 export function initGlobalAPI (Vue: GlobalAPI) {
   // config
   const configDef = {}
   configDef.get = () => config
+  // 非开发模式vue会提示所有非合法的调用方式，所以建议开发的时候一定要在开发模式下进行
   if (process.env.NODE_ENV !== 'production') {
     configDef.set = () => {
       warn(
@@ -43,7 +48,12 @@ export function initGlobalAPI (Vue: GlobalAPI) {
   Vue.delete = del
   Vue.nextTick = nextTick
 
+  // Object.create(null) 和 new Object() 的不同之处在于
+  // Object.create(null) 会创建一个真空的 {} 对象，连原型都没有
+  // 而 new Object() 则会创建一个拥有原型的 空对象
   Vue.options = Object.create(null)
+
+  // 为Vue.options新增 components / directives / filters 对象
   config._assetTypes.forEach(type => {
     Vue.options[type + 's'] = Object.create(null)
   })
@@ -51,7 +61,7 @@ export function initGlobalAPI (Vue: GlobalAPI) {
   // this is used to identify the "base" constructor to extend all plain-object
   // components with in Weex's multi-instance scenarios.
   Vue.options._base = Vue
-
+  // 给 components 扩展 KeepAlive:Object 属性
   extend(Vue.options.components, builtInComponents)
 
   initUse(Vue)
--- src/core/global-api/mixin.js
+++ src/core/global-api/mixin.js
@@ -2,6 +2,8 @@
 
 import { mergeOptions } from '../util/index'
 
+// Vue.mixin方法会将传入的mixin的所有方法、对象，全部都merge到当前Vue实例中。
+// 比如如果大部分地方都要监听窗口大小的改变，则利用mixin就可以。
 export function initMixin (Vue: GlobalAPI) {
   Vue.mixin = function (mixin: Object) {
     this.options = mergeOptions(this.options, mixin)
